759e8f9e14fc7c4c1baafcbe7ec8edb5b60b1869
aliatry1 <30816397+aliatry1@users.noreply.github.com>
2019-05-31 16:43:13 +0900

    Testing lambda package format

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('This is the latest with /var/lib/jenkins/workspace/lambda_deploy_test/test.zip!'),
+        body: JSON.stringify('Testing lambda package format'),
     };
     return response;
 };
