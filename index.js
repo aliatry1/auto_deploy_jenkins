86303cbd94d114e1d188882139a091f0901f4908
aliatry1 <30816397+aliatry1@users.noreply.github.com>
2019-05-31 15:23:55 +0900

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('This is a test!'),
+        body: JSON.stringify('This is a test----2!'),
     };
     return response;
 };
