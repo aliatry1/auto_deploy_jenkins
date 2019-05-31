4934d9fcaf5eb833beb2ecff983fca6eee3f3d49
aliatry1 <30816397+aliatry1@users.noreply.github.com>
2019-05-31 15:50:38 +0900

    jenkins/workspace/lambda_deploy_test/test.zip

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('This is a test 333!'),
+        body: JSON.stringify('This is the latest with /var/lib/jenkins/workspace/lambda_deploy_test/test.zip!'),
     };
     return response;
 };
