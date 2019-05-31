bfe14b9794b869529b57e3528e339fee8f6c4caa
aliatry1 <30816397+aliatry1@users.noreply.github.com>
2019-05-31 15:45:52 +0900

    test with curl -L -o test.zip

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('This is a test----2!'),
+        body: JSON.stringify('This is a test 333!'),
     };
     return response;
 };
