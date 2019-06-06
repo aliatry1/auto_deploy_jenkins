f63116aa06ec17752e43db806720725168dc8de2
aliatry1 <30816397+aliatry1@users.noreply.github.com>
2019-06-06 15:38:38 +0900

    Test from git after successful terraform result in jenkins server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Test to run the terrform 1'),
+        body: JSON.stringify('This was deployed from Github commit'),
     };
     return response;
 };
