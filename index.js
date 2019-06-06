8e6d07978bd43a3af3abd5e5e33993d2ff22fd7d
aliatry1 <30816397+aliatry1@users.noreply.github.com>
2019-06-06 11:04:26 +0900

    Test to run the terrform 1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Test to run the terrform'),
+        body: JSON.stringify('Test to run the terrform 1'),
     };
     return response;
 };
