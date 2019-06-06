a6644bb78e5815c43c75a77b6c1af0f9ebb05b3e
aliatry1 <30816397+aliatry1@users.noreply.github.com>
2019-06-06 17:14:01 +0900

    zip files name change so Ter can recognize change

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('This was deployed from Github commit Test number 2'),
+        body: JSON.stringify('Last terraform test'),
     };
     return response;
 };
+
+//Steps to prepare the terraform executions 
+//1. terraform init
+//2. .terraform file's owner must be ec2-user for the permision to install aws provider
+//3. A logic to rename the zip file beccause when terraform plan runs. since the zip file's name for lambda deploment is the same, it doesnt recognize the change in the package and will skip the terrafom apply command.
+//terraform plan -lock=false
+//terraform apply -lock=false -auto-approve
